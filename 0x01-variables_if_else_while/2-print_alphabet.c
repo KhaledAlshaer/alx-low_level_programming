7edd12417b916e1c1b24460a749347395390b409
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-11 11:17:57 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main = This is the main function
+ *main - This is the main function
  *Return: always returns 0
  */
 int main(void)
 {
-	for (int i = 'a'; i <= 'z'; i++)
+	for (char i = 'a'; i <= 'z'; i++)
 	{
 	putchar(i);
 	putchar('\n');
