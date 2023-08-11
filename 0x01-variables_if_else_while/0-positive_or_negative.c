aa3588e02e811fc2a1856952c4e9e1a56c306e32
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-11 09:34:13 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  *main - This is the main function
  *Return: always returns 0
- */ 
+ */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
