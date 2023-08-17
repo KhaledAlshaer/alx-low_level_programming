61387b247ca6501a12e7df17bd701425a93ca84c
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-17 12:36:16 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *more_numbers - This is the function
+  *Return: always returns 0
+  */
+void more_numbers(void)
+{
+	int i, j, n;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			n = j;
+			if (j > 9)
+			{
+				_putchar((n / 10) + 48);
+			}
+			_putchar((n % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
