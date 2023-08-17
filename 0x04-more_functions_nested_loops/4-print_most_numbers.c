27f143fb1e0e31607c0dcacc4d49d937589ba78d
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-17 09:04:06 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - This is the function
+ *@i: this is the variable
+ *Return: always returns 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+
+}
