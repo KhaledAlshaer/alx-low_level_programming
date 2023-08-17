019670051231ce78dfee0c49062d9f91d3f451c7
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-17 07:41:18 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - This is the main function
+ *Return: always returns 0
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	if (i >= 65 && i <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
