ff9e0f0953fa34ea3ed64a4294876baaa06e29d2
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-21 09:47:00 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_rev - This is the function
+ *@s: this is a variable
+ *Return: returns 0
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	
+		while (i--)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
