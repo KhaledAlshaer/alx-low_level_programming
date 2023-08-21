01139ad09f3dccd4abb575e5337e068ed0d16dae
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-21 12:50:16 -0700

    6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,7 +5,6 @@
  *@s: this is a variable
  *Return: returns 0
 */
-int _putchar(char c);
 void rev_string(char *s)
 {
 	int i, j, n = 0;
@@ -22,15 +21,4 @@ void rev_string(char *s)
 		s[n - 1 - j] = c;
 	}
 	_putchar('\n');
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; str++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 	}
+
 	_putchar('\n');
 }
