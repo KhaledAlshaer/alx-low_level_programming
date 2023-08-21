2c8f1f0ddf8a64b598ffe49dc2d1b43d5650bd65
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-21 14:03:04 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,14 +13,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; a[i] < n; i++)
 	{
-		if (i == n - 1)
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
-			printf("%d", a[i]);
-		}
-		else
-		{
-			printf("%d, ", a[i]);
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
 
