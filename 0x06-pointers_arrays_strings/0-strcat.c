51882457a14fc42b994a57e2c3e96ac8c6d3e0ca
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-08-23 11:35:05 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,19 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i, j, k, n = 0, c = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-		_putchar(dest[i]);
+		n++;
 	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		_putchar(src[j]);
+		c++;
+	}
+	for (k = 0 ; k <= c; k++)
+	{
+		dest[n + k] = src[k];
 	}
 	return (dest);
 }
