4dba1670a549f864f2797ffaf384970dfe0ab190
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-01 12:00:13 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - This is the function
+ *@argc: this is a variable
+ *@argv: this is a variable
+ *Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
