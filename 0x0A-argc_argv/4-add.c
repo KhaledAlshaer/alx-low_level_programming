81551161056a3c2d3e3a40572020d3f391a23808
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-01 12:54:23 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_digit(char *s);
+
 /**
- *main - This is the function
- *@argc: this is a variable
- *@argv: this is a variable
- *Return: returns 0
-*/
+ * main - adds positive numbers
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	char *k;
+	int i;
 
-	while (--argc)
+	if (argc == 1)
 	{
-		for (k = argv[argc]; *k; k++)
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_digit(argv[i]))
 		{
-			if (*k < '0' || *k > '9')
-			{
-				return (printf("Error\n"), 1);
-			}
-			else
-			{
-				sum += atoi(argv[argc]);
-			}
+			printf("Error\n");
+			return (1);
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
+}
+
+/**
+ * is_digit - checks if a string is a positive number
+ * @s: The string to check
+ *
+ * Return: 1 if the string is a positive number, 0 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+
+	return (1);
 }
