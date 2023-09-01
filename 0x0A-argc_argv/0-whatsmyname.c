45ffd17a8a55c6326990f7774ea5b3936155799b
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-01 11:45:42 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc ,char* argv[])
+/**
+ *main - This is the function
+ *@argc: this is a variable
+ *@argv: this is a variable
+ *Return: returns 0
+*/
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
