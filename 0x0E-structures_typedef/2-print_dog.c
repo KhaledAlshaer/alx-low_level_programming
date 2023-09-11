beb5529944adeae9bd9cb7c6dc6a9fd89ffa5a80
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-11 15:19:01 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,14 @@
 /**
  *print_dog - This is the function
  *@d: this is a variable
+ *Return: void
 */
 void print_dog(struct dog *d)
 {
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
 }
