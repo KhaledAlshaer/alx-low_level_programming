020c07c4cc5c6c172a0097e96b30ba202f15183c
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-13 14:25:29 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,31 +1,38 @@
 #include "calc.h"
 
 /**
- *main - This is the function
- *@argc: this is a variable
- *@argv: this is a variable
- *Return: returns 0
-*/
+ * main - This is the function
+ * @argc: this is a variable
+ * @argv: this is a variable
+ * Return: returns 0
+ */
 int main(int argc, char **argv)
 {
 	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
 	{
-		printf("Error\n"), exit(98);
+	printf("Error\n");
+	exit(98);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	op_func = get_op_func(argv[2]);
+
 	if (!op_func)
 	{
-		printf("Error\n"), exit(99);
+	printf("Error\n");
+	exit(99);
 	}
+
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		{
-			printf("Error");
-			exit(100);
-		}
+	{
+	printf("Error\n");
+	exit(100);
+	}
+
 	printf("%d\n", op_func(a, b));
 	return (0);
 }
+
