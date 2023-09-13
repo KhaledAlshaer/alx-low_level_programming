b47197333aa27483041879a13d49c93581596cf7
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-13 14:17:39 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ *op_add - This is the function
+ *@a: this is a variable
+ *@b: this is a variable
+ *Return: returns 0
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - This is the function
+ *@a: this is a variable
+ *@b: this is a variable
+ *Return: returns 0
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - This is the function
+ *@a: this is a variable
+ *@b: this is a variable
+ *Return: returns 0
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - This is the function
+ *@a: this is a variable
+ *@b: this is a variable
+ *Return: returns 0
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - This is the function
+ *@a: this is a variable
+ *@b: this is a variable
+ *Return: returns 0
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
