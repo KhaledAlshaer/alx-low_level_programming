fdfa376014725a3086da896014e7b122a022e843
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-14 10:56:06 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,56 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+
+/**
+ *format_char - This is the function
+ *@separator: this is a variable
+ *@ap: this is a variable
+ *Return: returns 0
+*/
+void format_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ *format_int - This is the function
+ *@separator: this is a variable
+ *@ap: this is a variable
+ *Return: returns 0
+*/
+void format_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ *format_float - This is the function
+ *@separator: this is a variable
+ *@ap: this is a variable
+ *Return: returns 0
+*/
+void format_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+ *format_string - This is the function
+ *@separator: this is a variable
+ *@ap: this is a variable
+ *Return: returns 0
+*/
+void format_string(char *separator, va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+	{
+		s = "(nil)";
+	}
+	printf("%s%s", separator, s);
+}
+
 /**
  *print_all - This is the function
  *@format: this is a variable
@@ -7,40 +58,38 @@
 */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	char *separator = "";
 	int i = 0;
+	char *separator = "";
+	va_list ap;
 
 	va_start(ap, format);
-
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
-		{
-			printf("%s%c", separator, va_arg(ap, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(ap, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, (float)va_arg(ap, double));
-		}
-		else if (format[i] == 's')
-		{
-			char *s = va_arg(ap, char *);
+	int j = 0;
 
-			if (s == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s%s", separator, s);
-			}
+	while (j < 4)
+	{
+		if (format[i] == "cifs"[j])
+		{
+		switch (format[i])
+		{
+			case 'c':
+				format_char(separator, ap);
+				break;
+			case 'i':
+				format_int(separator, ap);
+				break;
+			case 'f':
+				format_float(separator, ap);
+				break;
+			case 's':
+				format_string(separator, ap);
+				break;
 		}
-		separator = ", ";
+			separator = ", ";
+			}
+			j++;
+		}
 		i++;
 	}
 	va_end(ap);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,19 @@
 
 #include <stdarg.h>
 #include <stddef.h>
+
+/**
+ *struct token - This is the function
+ *@f: this is a variable
+ *@token: this is a variable
+ *Return: returns 0
+*/
+typedef struct format
+{
+	char *format;
+	void (*f)(char *, va_list);
+} format_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
