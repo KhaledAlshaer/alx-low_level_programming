11a95c4da172f15e3bde855ef853838410f472fc
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-26 10:51:35 -0700

    3

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ **add_nodeint - This is the function
+ *@head: this is a variable
+ *n@: this is a variable
+ *Return: returns 0
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ **add_nodeint_end - This is the function
+ *@head: this is a variable
+ *@n: this is a variable
+ *Return: returns 0
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *current;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->next = NULL;
+	new->n = n;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+
+		current = new;
+	}
+
+	return (new);
+}
