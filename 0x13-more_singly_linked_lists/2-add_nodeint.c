acc2c979242d1d0f27bfb01f2aeda27ace03976e
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-26 11:26:06 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 /**
  **add_nodeint - This is the function
  *@head: this is a variable
- *n@: this is a variable
+ *@n: this is a variable
  *Return: returns 0
 */
 listint_t *add_nodeint(listint_t **head, const int n)
