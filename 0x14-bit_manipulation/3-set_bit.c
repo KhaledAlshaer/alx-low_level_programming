26d6b075e745b8a9fab05eae794c5e2908ed2098
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-28 08:39:27 -0700

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *set_bit - This is the function
+ *@n: this is a variable
+ *@index: this is a variable
+ *Return: returns 0
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n = *n | (1UL << index);
+	return (1);
+}
