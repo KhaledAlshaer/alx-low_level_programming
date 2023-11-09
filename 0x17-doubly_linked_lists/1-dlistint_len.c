5fa301d21765c2900f0aea761bcb9363aa2def8b
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-11-09 08:41:09 -0800

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - this is the function
+ *@h: this is a vraiable
+ *Return: return 0
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
