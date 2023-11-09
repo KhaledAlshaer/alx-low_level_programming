6f5b6a79a536dad854e887632520d9509cbc8f72
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-11-09 09:13:13 -0800

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,9 @@
-#include "list.h"
+#include "lists.h"
+
 /**
  *free_dlistint - this is the function
  *@head: this is a vraiable
-  *Return: return 0
+ *Return: return 0
 */
 void free_dlistint(dlistint_t *head)
 {
