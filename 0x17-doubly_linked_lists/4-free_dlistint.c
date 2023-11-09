477715abe5074a97039bdbe072a427667de54e02
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-11-09 09:09:36 -0800

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+/**
+ *free_dlistint - this is the function
+ *@head: this is a vraiable
+  *Return: return 0
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
