2c97ea5447113afe80c889c460057dff9af77721
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-05-23 02:35:38 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (array[i] != value && i <= size)
+	while (array[i] != value && i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		i++;
